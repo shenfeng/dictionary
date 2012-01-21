6cb9c19b1691136a6435ec974b5a105b9e52301c
feng <feng.shen@onycloud.com>
2012-01-21 14:17:11 +0800

    add c impl search

---
+++ search.c
@@ -0,0 +1,83 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define FILENAME "/tmp/dbdata"
+
+off_t get_file_size (int fd) {
+    struct stat statbuf;
+    fstat(fd, &statbuf);
+    off_t length = statbuf.st_size;
+    return length;
+}
+
+int get_unsigned(char b) {
+    if (b >= 0) return b;
+    else { return 256 + b; }
+}
+
+int read_short(char *buf, int offset) {
+    int hi = get_unsigned(buf[offset]);
+    int low = get_unsigned(buf[offset+1]);
+    return (hi << 8) + low;
+}
+
+int* build_index(char *data, int word_count, int total_length){
+    int* index_data = (int*)malloc(sizeof(int) * word_count);
+    int word_index = 0, index = 2; // ignore first 2 byte, that's word count
+    while(index < total_length) {
+        index_data[word_index++] = index;
+        while(data[index++]);   // skip word, word is terminated by \0
+        index += read_short(data, index) + 2;
+    }
+    return index_data;
+}
+
+// return index in data, or -1 if not found, target is all lowercase
+int search_word(char *data, int* index_data, int word_count, char* target) {
+    int low = 0, high = word_count - 1;
+    while(low <= high) {
+        int mid = (low + high) >> 1;
+        int cmp = strcmp(target, data + index_data[mid]);
+        // printf("%s, %s, %d, mid %d\n", target, data + index_data[mid], cmp, mid);
+        if(cmp > 0) {
+            low = mid + 1;
+        } else if (cmp < 0) {
+            high = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+void test_search(char *data, int *index_data, int word_count) {
+    char * targets[] = {
+        "yuppify", "bird-watcher", "arthritis", "ali, muhammad", "-monger",
+        "yukon", "women's studies"
+    };
+    for(int i = 0; i < 7; ++i) {
+        int result = search_word(data, index_data, word_count, targets[i]);
+        printf("%s, %d\n",targets[i], result);
+    }
+}
+
+int main(int argc, char** argv) {
+    int fd = open(FILENAME, O_RDONLY);
+    if(fd <= 0) { perror(FILENAME); exit(1); }
+    off_t length = get_file_size(fd);
+    char *data = mmap(NULL, length, PROT_READ, MAP_SHARED, fd, 0);
+    close(fd);
+
+    int word_count = read_short(data, 0);
+    printf("file: %s, size: %lu\n", FILENAME, length);
+    printf("word count: %d\n", word_count);
+    int *index = build_index(data, word_count, length);
+    // test_search(data, index, word_count); //
+    return 0;
+}
